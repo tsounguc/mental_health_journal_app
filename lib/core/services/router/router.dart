1db0b0f5122a564292b59d70f394de03d90f78fe
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-01-04 10:50:09 -0500

    Add router. Create resources folder and sign_in_screen.dart file

---
+++ lib/core/services/router/router.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health_journal_app/features/auth/presentation/views/sign_in_screen.dart';
+
+part 'router.main.dart';
---
+++ lib/core/services/router/router.main.dart
@@ -0,0 +1,31 @@
+part of 'router.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  final arguments = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      return _pageBuilder(
+        (_) => const SignInScreen(),
+        settings: settings,
+      );
+    default:
+      return _pageBuilder(
+        (_) => const Scaffold(),
+        settings: settings,
+      );
+  }
+}
+
+PageRouteBuilder<dynamic> _pageBuilder(
+  Widget Function(BuildContext context) page, {
+  required RouteSettings settings,
+}) {
+  return PageRouteBuilder(
+    settings: settings,
+    transitionsBuilder: (_, animation, __, child) => FadeTransition(
+      opacity: animation,
+      child: child,
+    ),
+    pageBuilder: (context, _, __) => page(context),
+  );
+}
